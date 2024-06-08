26b19f338d1588df538ab9db33ad9b9fe504f59c
bonghanwith2 <119918124+bonghanis@users.noreply.github.com>
2024-06-08 22:40:01 +0900

    update title

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st
 from openai import OpenAI
 
 # 페이지 제목 설정
-st.title("친근한 챗봇")
+st.title("친근한 챗봇 update 1 ")
 
-# 사용자로부터 API 키 입력 받기
 api_key = st.text_input("API 키를 입력하세요:", type="password")
 
-# 입력된 API 키를 세션 상태에 저장
 if api_key:
     st.session_state["api_key"] = api_key
 
@@ -15,7 +13,6 @@ if api_key:
 if "api_key" in st.session_state:
     client = OpenAI(api_key=st.session_state["api_key"])
 
-# 챗봇 설정 메시지
 system_message = '''
 너의 이름은 친구봇이야.
 너는 항상 반말을 하는 챗봇이야. 다나까나 요 같은 높임말로 절대로 끝내지 마
@@ -35,11 +32,11 @@ for idx, message in enumerate(st.session_state.messages):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-# 사용자 입력 받기
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+
 
     # OpenAI 모델 호출
     if "api_key" in st.session_state:
@@ -54,3 +51,5 @@ if prompt := st.chat_input("What is up?"):
             )
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+        print(st.session_state.messages)
